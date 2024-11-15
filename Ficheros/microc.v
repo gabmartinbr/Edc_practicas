a19bdf99033d98eb669e8f4d471aea1b194fa1ae
brigafurs <alu0101539157@ull.edu.es>
2024-11-15 20:55:46 +0000

    microprocesador hecho, falta testbench

--- Ficheros/microc.v
+++ Ficheros/microc.v
@@ -6,12 +6,81 @@ module microc(
   );
 //Microcontrolador sin memoria de datos de un solo ciclo
 
-// declaración de señales internas
+// declaración señales internas
 reg [9:0] PC;             // registro de program counter 10b
 wire [15:0] instruc;      // instruccion a usar 16b
 wire [7:0] reg_1, reg_2;  // registros de los operandos 8b
 wire [7:0] alu_output;    // registro resultado alu 8b
+wire [7:0] dato_a_reg;    // wd3, dato a escribir en los registros
 wire [9:0] sig_pc;        // dirección del siguiente program counter 10b
+wire z_flag;              // salida, flag de zero de el flipflop
 
+// instanciar los módulos
+// instancia del sumador para siguiente dir del pc
+sum sumador_pc (
+  pc_siguiente,   // salida del sumador pc incrementado sig dir del pc
+  10'b1           // entrada 0(a), constante 1 para la suma
+  PC,             // entrada 1(b), valor actual del pc
+);
 
+// instancia de multiplexor output -> program counter
+mux2 #(10) mux_a_pc (
+  sig_pc,       // salida mux que va al pc
+  instruc[9:0], // entrada 0, dir salto
+  pc_siguiente, // entrada 1, salida del sumador_pc, siguiente dir pc
+  s_inc         // 0 = saltar  1 = incrementar
+);
+
+// instancia del registro del PC
+registro #(10) registro_PC (
+  PC,           // valor actual del pc, salida pc
+  clk,          // señal reloj
+  reset,        // señar reseteo
+  sig_pc        // salida del mux, entrada del registro pc
+);
+
+// instancia de memoria de programa
+memprog memoria_prog (
+  instruc,    // salida, instrucción actual 16b
+  clk,        // señal reloj
+  PC          // entrada pc actual
+);
+
+// instancia multiplexor output -> banco de registros
+mux2 #(8) mux_a_banco_reg (
+  dato_a_reg,       // salida, dato a escribir en el registro
+  alu_output,       // entrada 0, salida alu
+  instruc [11:4],   // entrada 1, carga de inmediato
+  s_inc             // 0 = operando reg_2  1 = cargar inmediato
+);
+
+// instancia banco de registros
+regfile banco_registros (
+  reg_1,            // salida 1 rd1
+  reg_2,            // salida 2 rd2
+  clk,              // señal de reloj
+  we3,              // señal habilitar escritura
+  instruc [11:8],   // ra1, dir primer reg leido
+  instruc [7:4],    // ra2, dir segundo  reg leido 
+  instruc [3:0],    // wa3, dir reg donde se va a escribir
+  dato_a_reg        // wd3, salida mux_a_banco_reg
+);
+
+// instancia alu
+alu alu_module (
+  alu_output,       // salida alu, entrada 0 mux
+  z,                // salida, flag cero
+  reg_1,            // entrada 0, operando 1 rd1
+  reg_2,            // entrada 1, operando 2 rd2
+  Op,               // selector de operación a realizar
+);
+
+// instancia flipflop
+ffd flipflop_zero (
+  clk,            // entrada, señal de reloj
+  reset,          // entrada, señal de reseteo
+  z,
+  wez,
+  z_flag
+);
 endmodule
--- Ficheros/microc_tb.v
+++ Ficheros/microc_tb.v
@@ -2,29 +2,75 @@
 module microc_tb;
 
 // declaración de variables
-//...
+reg clk, reset;
+reg s_inc, s_inm, we3, wez;
+reg [2:0] Op;
+wire [5:0] Opcode;
+wire z;
+wire [9:0] PC;
+wire [7:0] reg_1, reg_2, alu_output, dato_a_reg;
+wire z_flag;
+wire [15:0] instruc;
 
 // instanciación del camino de datos
-//...
+microc micro_procesador (
+  Opcode, 
+  z,
+  clk,
+  reset,
+  s_inc,
+  s_inm,
+  we3,
+  wez,
+  Op
+);
 
 // generación de reloj clk
-//...
-
-// Reseteo y configuración de salidas del testbench
-initial
-begin
-  $dumpfile("microc_tb.vcd");
-  $dumpvars;
-  // ... señal de reset
+always begin
+  clk = 0; #5;
+  clk = 1; #5;
 end
 
-// Bloque simulación señales control por ciclo
-initial
-begin
-  // retardos y señales para ejecutar primera instrucción (ciclo 1)
-  // retardos y señales para ejecutar segunda instrucción (ciclo 2)
-  // ...
-  $finish;
+// Reseteo y configuración de salidas del testbench
+initial begin
+    $dumpfile("microc_tb.vcd");
+    $dumpvars(0, microc_tb);  // Volcado de señales para ver en GTKWave
+
+    // Inicializamos las señales
+    clk = 0;
+    reset = 1;  // Activar el reset al principio
+    s_inc = 0;  // No incrementar el PC inicialmente
+    s_inm = 0;  // No usar valor inmediato
+    we3 = 0;    // No habilitar escritura en registros
+    wez = 0;    // No habilitar el flag de cero
+    Op = 3'b000; // Operación de la ALU inicial (puedes ajustarlo según las pruebas)
+
+    // Desactivamos el reset después de 10 ns
+    #10 reset = 0;
+end
+
+// Bloque de simulación: señales control por ciclo
+initial begin
+    // Primer ciclo: ejecutar una instrucción (por ejemplo, cargar un valor)
+    #20 s_inc = 1;    // Incrementar el PC (ciclo 1)
+    Op = 3'b010;      // Operación de la ALU, por ejemplo, ADD
+    we3 = 1;          // Habilitar escritura en el banco de registros
+    s_inm = 1;        // Usar valor inmediato
+    wez = 1;          // Actualizar bandera de cero
+    #20;              // Esperar un ciclo de reloj
+    // Aquí puedes observar cómo se comportan las señales y el PC
+
+    // Segundo ciclo: ejecutar otra instrucción (por ejemplo, una suma)
+    #20 s_inc = 1;    // Incrementar el PC
+    Op = 3'b010;      // ALU realiza una suma
+    we3 = 1;          // Habilitar escritura en el banco de registros
+    s_inm = 0;        // Usar registros en lugar de inmediato
+    wez = 1;          // Actualizar bandera de cero
+    #20;              // Esperar un ciclo de reloj
+
+    // Más ciclos de prueba, puedes agregar más instrucciones aquí.
+
+    $finish;          // Termina la simulación
 end
 
 endmodule
